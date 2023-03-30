d9d09e91a62de8a1aae8068a35d0139515e297db
Xamel7 <123972550+Xamel7@users.noreply.github.com>
2023-03-30 10:56:05 -0500

    Hoops

---
+++ game.js
@@ -0,0 +1,10 @@
+function Player (Pname, age, height, weight, twoPtShootingPercentage, threePtShootingPercentage, freeThrowPercentage);
+this.Pname = Pname;
+this.age = age;
+this.weight = weight;
+this.height = height;
+this.twoPtShootingPercentage = twoPtShootingPercentage;
+this.threePtShootingPercentage = threePtShootingPercentage;
+this.freeShootingPercentage = freeShootingPercentage;
+
+let new Player = Player1
